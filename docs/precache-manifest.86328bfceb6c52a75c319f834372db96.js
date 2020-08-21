0e2969eb79105c996058c7cc9067c1738ff9da84
Oscar Navarrete <oscar.navarrete@outlook.com>
2020-08-21 15:39:36 -0400

    docs agregado

---
+++ docs/precache-manifest.86328bfceb6c52a75c319f834372db96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5011a1947a5cb2c455d551af8e2a7557",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4530202ff236b1e1021",
+    "url": "/static/css/main.f88aa21a.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4530202ff236b1e1021",
+    "url": "/static/js/main.31d6b40c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
